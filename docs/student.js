ef2e41cc369c62094f228469b6e18b47e94c7080
Faysal Ahmed Bappy <faysalahmed.ces.98@gmail.com>
2024-12-19 18:42:03 +0600

    student get by roll range api update

--- docs/student.js
+++ docs/student.js
@@ -7,9 +7,11 @@ const studentDocs = {
         tags: ["Student"],
         summary: "Get students by roll number range",
         description: "Get students by roll number range",
-        security: [{
-          Bearer: []
-        }],
+        security: [
+          {
+            Bearer: [],
+          },
+        ],
         requestBody: {
           required: true,
           content: {
@@ -19,34 +21,39 @@ const studentDocs = {
                 properties: {
                   startRoll: { type: "number", example: 1 },
                   endRoll: { type: "number", example: 20 },
-                  class:{type:"string",example: "4" },
-                  section:{type:"string",example: "A" },
-                  shift:{type:"string",example: "Morning" },
-                  subject:{type:"string",example: "English 1st Paper" },
-
+                  class: { type: "string", example: "4" },
+                  section: { type: "string", example: "A" },
+                  shift: { type: "string", example: "Morning" },
+                  subject: { type: "string", example: "English 1st Paper" },
+                  year:{
+                    type:"string", example:"2024"
+                  },
+                  subject:{
+                    type:"string", example:"English"
+                  }
                 },
               },
             },
           },
         },
         responses: {
-            200: {
-                description: "Students fetched successfully",
-                content: {
-                    "application/json": {
-                        schema: {
-                            type: "object",
-                            properties: {
-                                data: { type: "array", items: { type: "object" } }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-      }
-    }
-  }
-}
+          200: {
+            description: "Students fetched successfully",
+            content: {
+              "application/json": {
+                schema: {
+                  type: "object",
+                  properties: {
+                    data: { type: "array", items: { type: "object" } },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+};
 
 module.exports = studentDocs;
